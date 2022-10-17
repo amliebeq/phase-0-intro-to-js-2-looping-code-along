66e1b5611ab0396fa0c5b8d8d64611a652ed96a0
alex lieberman <alex.lieberman182@gmail.com>
2022-10-16 17:46:40 -0700

    commit tree 3cbb2751b7ad2e674dc569399d9924a613ed2433

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(number) {
+    let i = number
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+function writeCards(name, event) {
+    let array = []
+    for (let i = 0; i < name.length; i++) {
+        array.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return array
+}
